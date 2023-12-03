e3fd0036de715cb583021e600b0ba4fcc7f0a5ec
eko saputra <ekosaputra1331@gmail.com>
2023-12-04 06:02:29 +0700

    init

---
+++ resources/js/Pages/UserManagement.jsx
@@ -0,0 +1,12 @@
+import { Button } from '@/Components/ui/button'
+import React from 'react'
+
+const UserManagement = () => {
+  return (
+    <div>
+        <Button>test</Button>
+    </div>
+  )
+}
+
+export default UserManagement
